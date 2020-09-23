eba7c0f92f55521e4ae3c7484148c91f6eb9e626
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-09-23 18:02:10 +0900

    :mortar_board: let式を勉強した

--- baby.hs
+++ baby.hs
@@ -83,3 +83,16 @@ bmiTell weight height
         -- normal = 25.0
         -- fat = 30.0 -- インデントは揃える
         (skinny, normal, fat) = (18.5, 25.0, 30.0)
+initials :: String -> String -> String
+initials firstname lastname = [f] ++ ". " ++ [l] ++ "."
+  where (f:_) = firstname
+        (l:_) = lastname
+calcBmis :: [(Double, Double)] -> [Double]
+-- calcBmis xs = [bmi w h | (w, h) <- xs]
+--   where bmi weight height = weight / height ^ 2
+cylinder :: Double -> Double -> Double
+cylinder r h =
+  let sideArea = 2 * pi * r * h
+      topArea = pi * r * 2
+  in  sideArea + 2 * topArea
+calcBmis xs = [bmi | (w, h) <- xs, let bmi = w / h ^ 2]
