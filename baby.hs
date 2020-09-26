63020d44da2e2385d57e932065dee886ef9eac9c
kokorinosoba <34607448+kokorinosoba@users.noreply.github.com>
2020-09-27 01:31:26 +0900

    :mortar_board: 再帰について勉強した

--- baby.hs
+++ baby.hs
@@ -140,3 +140,49 @@ describeList ls =
       [] -> "empty."
       [x] -> "a singleton list."
       xs -> "a longer list."
+
+fibonacci :: Int -> Int
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list!"
+maximum' [x] = x
+maximum' (x : xs) = max x (maximum' xs)
+
+replicate' :: Int -> a -> [a]
+replicate' n x
+  | n <= 0 = []
+  | otherwise = x : replicate' (n - 1) x
+
+take' :: Int -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x : xs) = x : take' (n - 1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x : xs) = reverse' xs ++ [x]
+
+repeat' :: a -> [a]
+repeat' x = x : repeat' x
+
+zip' :: [a] -> [b] -> [(a, b)]
+zip' _ [] = []
+zip' [] _ = []
+zip' (x : xs) (y : ys) = (x, y) : zip' xs ys
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' a [] = False
+elem' a (x : xs)
+  | a == x = True
+  | otherwise = a `elem'` xs
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x : xs) = quicksort smallerOrEqual ++ [x] ++ quicksort larger
+  where
+    smallerOrEqual = [a | a <- xs, a <= x]
+    larger = [a | a <- xs, a > x]
